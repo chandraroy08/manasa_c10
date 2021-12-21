a872a920cd0a98584bc9a5fa1f6ae70bcdbeb026
chandraroy08 <68724499+chandraroy08@users.noreply.github.com>
2021-12-21 16:36:41 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,26 @@
+
+ 
+ 
+ 
+ function preload(){
+
+ 
+ }
+ 
+ 
+ 
+ 
+ function setup() {
+  createCanvas(600, 200);
+ 
+  
+ }
+function draw() {
+  background(220);
+
+  
+ 
+ 
+ 
+  drawSprites();
+}
